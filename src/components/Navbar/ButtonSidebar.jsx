d3a269a398206937891d3112a6e0e278e4e537a6
Santigumi <santymillan95@gmail.com>
2025-10-20 22:31:52 -0500

    sidebar

---
+++ src/components/Navbar/ButtonSidebar.jsx
@@ -0,0 +1,9 @@
+
+const ButtonSidebar = ({icon, name, state}) => {
+    return(
+        <button
+        class="bg-transparent"
+        >{icon}{name}</button>
+    )
+}
+export default ButtonSidebar
