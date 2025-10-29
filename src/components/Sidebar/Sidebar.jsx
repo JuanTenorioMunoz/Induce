4bd532bf3be431785fd8176be875f2649d90d115
Santigumi <santymillan95@gmail.com>
2025-10-29 11:54:20 -0500

    logo induce added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,12 @@
 import ButtonSidebar from "./ButtonSidebar";
+import logoInduce from "../../assets/Logo_induce.png";
 
 const Sidebar = () => {
     return(
         <div className="flex flex-col">
+            <div className="mb-6" style={{ width: '200px' }}>
+                <img src={logoInduce} alt="Logo Induce" style={{ width: '100%', height: 'auto', display: 'block' }} />
+            </div>
             <ButtonSidebar icon="bi-house-fill" name="Inicio"></ButtonSidebar>
             <ButtonSidebar icon="bi-person-fill" name="Mi perfil"></ButtonSidebar>
             <ButtonSidebar icon="bi-briefcase-fill" name="Mis vacantes"></ButtonSidebar>
