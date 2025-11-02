2442510c3c78e38d5ca2067379edc1db2c8a824c
Santigumi <santymillan95@gmail.com>
2025-11-02 15:42:38 -0500

    add routes

--- src/components/Sidebar/ButtonSidebar.jsx
+++ src/components/Sidebar/ButtonSidebar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const ButtonSidebar = ({icon, name, to}) => {
     return(
         <Link to={to || "#"} className="sidebar-button">
-            <i className={`bi ${icon} sidebar-icon`}></i>
+            <i className={`bi bi-${icon} sidebar-icon`}></i>
             <span>{name}</span>
         </Link>
     )
