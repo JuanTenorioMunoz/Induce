7758a8d0111e9233f19e2938776b54c60d195537
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-11-02 16:13:30 -0500

    add provisional JobCard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import ChatWidget from '../../components/ChatAI/ChatWidget';
+import JobCard from '../../components/JobCard/JobCard';
 
 const Home = () => {
   
   return (
     <>
+    <JobCard></JobCard>
     <ChatWidget></ChatWidget>
     </>
   );
