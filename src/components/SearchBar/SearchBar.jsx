c6a8c64ee1d470d7057b7c1b9350cc5a501fa813
Santigumi <santymillan95@gmail.com>
2025-11-02 18:15:03 -0500

    w style

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 const SearchBar = () => {
     return(
-        <div className="flex items-center px-[24px] py-[16px] rounded-[12px] border border-[var(--color-violet_blue_500)] w-64 h-40">
+        <div className=" w-1/4 flex items-center px-[24px] py-[16px] rounded-[12px] border border-[var(--color-violet_blue_500)]">
             <i className="bi bi-search mr-2 text-[var(--color-violet_blue_500)]"></i>
             <input type="text" placeholder="Buscar aquí..." className="flex-1 border-none outline-none bg-transparent focus:outline-none"></input>
         </div>
