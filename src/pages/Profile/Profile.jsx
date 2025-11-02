7792bb19b2a0c982ad2cb9c297f2378ce66840a1
Santigumi <santymillan95@gmail.com>
2025-11-02 18:07:11 -0500

    add styles

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,15 @@
-
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 
 const Profile = () => {
     return(
-        <>
-        </>
+        <div className="profile-page-container flex flex-row h-screen w-full">
+        <Sidebar />
+        <div className="profile-container flex flex-col h-screen w-full">
+        <SearchBar />
+        </div>
+        </div>
     )
 }
 
