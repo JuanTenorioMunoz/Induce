f1ce4c3eba2577288733c08e6ea6ef10df3ee643
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-11-03 14:39:20 -0500

    fix border shadow

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ChatOption from './../ChatAI/ChatOption';
+import ChatOption from "./../ChatAI/ChatOption";
 
 export const JobCard = ({
   recommended,
@@ -13,7 +13,13 @@ export const JobCard = ({
   timeAgo,
 }) => {
   return (
-    <article className="flex flex-col w-full max-w-[675px] p-4 bg-[var(--color-alice_blue)] border border-[var(--color-violet_blue_light)] rounded-lg shadow-sm font-primary">
+    <article
+      className="
+        flex flex-col w-full max-w-[675px] p-4 
+        bg-[var(--color-alice_blue)] rounded-lg font-primary
+        shadow-[var(--shadow-cm)]
+      "
+    >
       <header className="flex items-center justify-between mb-3">
         <div className="flex items-center gap-2">
           <div className="w-5 h-5 bg-gray-300 rounded-sm" />
@@ -44,7 +50,7 @@ export const JobCard = ({
             )}
             <button
               aria-label="Guardar trabajo"
-              className="w-8 h-8 border rounded-md border-[var(--color-violet_blue_light)] flex items-center justify-center"
+              className="w-8 h-8 rounded-md flex items-center justify-center border border-transparent"
             >
               <div className="w-4 h-4 bg-gray-300" />
             </button>
