12038f0c7453b4d731a0392b977def7ae753314f
Santigumi <santymillan95@gmail.com>
2025-11-03 18:28:54 -0500

    fix problem with tailwind version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors: {
-        // Tus colores personalizados con código hex
-        'mi-azul': '#3b82f6',
-        'mi-verde': '#22c55e',
-        'mi-rojo': '#ef4444',
-        'mi-gris': '#64748b',
-        'mi-morado': '#8b5cf6',
-      }
     },
   },
   plugins: [],
-}
+};
