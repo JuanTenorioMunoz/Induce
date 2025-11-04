06d1340d1070cd9b0fb55377ec515f1a98b63641
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-11-04 11:57:55 -0500

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import './App.css'
-import Home from './pages/Home/Home'
-import Profile from './pages/Profile/Profile'
-import LandingPage from './pages/LandingPage/LandingPage'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Router from './routes/Router'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/profile" element={<Profile/>}/>
-                <Route path="/landing" element={<LandingPage/>}/>
-                
-            </Routes>
-        </BrowserRouter>
+      <Router></Router>
     </>
   )
 }
