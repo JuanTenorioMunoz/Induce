7c8d284a94a52972e91c6ae24cd9a91f482465f5
Santigumi <santymillan95@gmail.com>
2025-11-03 20:48:19 -0500

    update styles

--- src/components/UserProfileContainer/UserProfileContainer.jsx
+++ src/components/UserProfileContainer/UserProfileContainer.jsx
@@ -1,6 +1,7 @@
 const UserProfileContainer = ({ image, name, label }) => {
   return (
-    <div className="inline-flex w-64 space-x-3 h-12 items-center ">
+    <div className="inline-flex w-64 space-x-3 h-12 items-center bg-[var(--color-violet-blue-200) border-2 border-[var(--color-violet-blue-200)]">´
+      <i className="bi bi-bell text-lg text-[var(--color-violet-blue)]"></i>
       <div className="w-12 h-12 rounded-full overflow-hidden flex-shrink-0">
         <img
           src={image}
@@ -12,6 +13,7 @@ const UserProfileContainer = ({ image, name, label }) => {
         <p className="font-bold text-sm truncate">{name}</p>
         <p className="text-xs font-light leading-none truncate">{label}</p>
       </div>
+      <i class="bi bi-caret-down text-lg text-[var(--color-violet-blue)]"></i>
     </div>
   );
 };
