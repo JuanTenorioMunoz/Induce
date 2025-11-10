00ae9fa45bc993d50dc1bffef4d16830a17d4a3a
Santigumi <santymillan95@gmail.com>
2025-11-09 23:40:09 -0500

    updating styles

--- src/components/Input_text/InputText.jsx
+++ src/components/Input_text/InputText.jsx
@@ -1,25 +1,21 @@
 
-const InputText = ({label, IconLeft, IconRight, placeholder}) => {
+const InputText = ({label, IconLeft, IconRight, Placeholder}) => {
     return(
-        <div className="w-full flex flex-col space-y-1">
-<label className="text-gray-800 font-medium">{label}</label>
+        <div className="flex flex-col space-y-1 w-76 text-md">
+        <label className="bg-(--color-text-titulos-y-texto-destacado) font-medium">{label}</label>
 
+        <div className="flex items-center bg-gray-50 border border-blue-200 rounded-xl px-3 py-2 shadow-sm focus-within:border-blue-400 transition">
+         <i className={`bi bi-${IconLeft}`} />
 
-<div className="flex items-center bg-gray-50 border border-blue-200 rounded-xl px-3 py-2 shadow-sm focus-within:border-blue-400 transition">
- <i className={`bi bi-${IconLeft}`} />
+        <input
+        type="text"
+        placeholder={Placeholder}
+        className="flex-1 bg-transparent text-lg px-4 outline-none text-(--color-texto-secundario)"
+        />
 
-
-<input
-type="text"
-placeholder={placeholder}
-className="flex-1 bg-transparent outline-none text-gray-700"
-/>
-
-
-<i className={`bi bi-${IconRight}`} />
-</div>
-</div>
-    )
-}
+        <i className={`bi bi-${IconRight}`} />
+        </div>
+        </div>
+    )};
 
 export default InputText
