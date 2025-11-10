889bfe230b083d74bbc7b33fbb6283cf64dfec3a
Santigumi <santymillan95@gmail.com>
2025-11-10 00:44:19 -0500

    create button link component and updating styles

---
+++ src/components/ButtonLink/ButtonLink.jsx
@@ -0,0 +1,14 @@
+const ButtonLink = ({direction, text, button}) => {
+    return(
+        <div className="flex flex-row w-64 justify-center gap-2">
+        <p className="text-(--color-elementos-claros)">{text}</p>
+        <a 
+        href={direction}
+        className="font-medium underline text-(--color-elementos-claros)">
+        {button}
+        </a>
+        </div>
+    )
+}
+
+export default ButtonLink
