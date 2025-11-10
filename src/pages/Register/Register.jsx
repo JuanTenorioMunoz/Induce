7000e90afea31f17ebee6fcfbd91eba5744d1611
Santigumi <santymillan95@gmail.com>
2025-11-10 00:31:06 -0500

    testing rectangule_button

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,6 @@
 import InputText from "../../components/Input_text/InputText"
 import Navbar from "../../components/Navbar/Navbar"
+import RectanguleButton from "../../components/Rectangule_button/RectanguleButton"
 const Register = () => {
     return(
         <div className="bg-(--color-background-alice-blue">
@@ -10,6 +11,11 @@ const Register = () => {
         IconLeft="person-fill"
         IconRight="bi bi-question-circle"
         ></InputText>
+        <RectanguleButton
+        icon="google"
+        text="Sign in with Google"
+        >
+        </RectanguleButton>
         </div>
     )
 }
