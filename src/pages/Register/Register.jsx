5f21cf74a175d9160706919ecbf576e03537d01d
Santigumi <santymillan95@gmail.com>
2025-11-09 23:02:43 -0500

    create register page

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../../components/Navbar/Navbar"
+const Register = () => {
+    return(
+        <div className="bg-white">
+        <Navbar></Navbar>
+        </div>
+    )
+}
+export default Register
