2277c484598d13a7fd649128670cf385507d789a
Santigumi <santymillan95@gmail.com>
2025-11-11 13:29:12 -0500

    navigate routes added

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,18 +1,28 @@
+import ButtonLink from "../../components/ButtonLink/ButtonLink"
+import InputText from "../../components/Input_text/InputText"
+import Navbar from "../../components/Navbar/Navbar"
+import RectanguleButton from "../../components/Rectangule_button/RectanguleButton"
+import Heading from "../../components/Titule/Titule"
+import RegisterImage from "../../assets/Register-image1.png"
+
+import { useNavigate } from "react-router-dom"
 
 const SignIn = () => {
+    const navigate = useNavigate()
     return(
-                <div className="bg-(--color-background-alice-blue) flex flex-col w-full min-h-screen overflow-y-auto">
+            <div className="bg-(--color-background-alice-blue) flex flex-col w-full h-screen">
             <Navbar></Navbar>
-        <div className="flex w-full min-h-screen">
+            <div className="flex w-full h-screen">
             <div className="w-1/2 flex flex-col justify-end items-center">
             <img src={RegisterImage}
             className="w-2/3"
             ></img>
             </div>
-            <div className="w-1/2 h-full flex flex-col justify-center items-center gap-5 my-10 box-content">
+            <div className="w-1/2 h-full flex flex-col justify-center items-center gap-12 box-content">
             <Heading
             titule="Iniciar sesión"
             ></Heading>
+            <div className="w-full flex flex-col justify-center items-center gap-4">
             <InputText
             IconLeft="envelope"
             IconRight="question-circle"
@@ -25,9 +35,13 @@ const SignIn = () => {
             label="Contraseña"
             Placeholder="Escribe tu constraseña"
             ></InputText>
+            <div className="flex flex-row justify-between items-center w-8/12 my-2">
+            <a href="#" className="underline text-xs text-(--color-elementos-claros)">¿Olvidaste tu contraseña?</a>
             <button 
+            onClick={() => navigate('/landing')}
             className="text-md cursor-pointer bg-(--color-violet-blue) text-(--color-fondos-oscuros) w-[40%] box-border px-2 py-1 rounded-md"
-            >Registrate</button>
+            >Iniciar sesión</button>
+            </div>
             <div className="flex items-center justify-center w-[60%]">
             <div className="h-px w-full bg-(--color-violet-blue)"></div>
             <span className="px-4 text-(--color-violet-blue)">o</span>
@@ -42,10 +56,11 @@ const SignIn = () => {
             text="Iniciar sesión con Microsoft"
             ></RectanguleButton>
             <ButtonLink
-            direction="#"
+            direction="/register"
             text="¿No tienes cuenta?"
             button="Registrate"
             ></ButtonLink>
+            </div>
             </div>
         </div>
         </div>
