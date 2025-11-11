8ff111409ef8c6c876d04b1286a239b24db9480c
Santigumi <santymillan95@gmail.com>
2025-11-11 09:31:29 -0500

    Create component titule

---
+++ src/components/Titule/Titule.jsx
@@ -0,0 +1,9 @@
+const Titule = ({titule}) => {
+    return(
+        <p 
+        className="text-5xl font-semibold text-(--color-elementos-claros)"
+        >{titule}</p>
+    )
+}
+
+export default Titule
