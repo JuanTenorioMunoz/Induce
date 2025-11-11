1471b491e5ffeeed01dc0c92dda052779ea1aa5c
Santigumi <santymillan95@gmail.com>
2025-11-11 12:37:08 -0500

    updating styles

--- src/components/Input_text/InputText.jsx
+++ src/components/Input_text/InputText.jsx
@@ -1,16 +1,16 @@
 
 const InputText = ({label, IconLeft, IconRight, Placeholder}) => {
     return(
-        <div className="flex flex-col space-y-1 w-76 text-md">
-        <label className="bg-(--color-text-titulos-y-texto-destacado) font-medium">{label}</label>
+        <div className="flex flex-col gap-1 w-[70%]">
+        <label className="bg-(--color-text-titulos-y-texto-destacado) text-xs">{label}</label>
 
         <div className="flex items-center bg-gray-50 border border-blue-200 rounded-xl px-3 py-2 shadow-sm focus-within:border-blue-400 transition">
-         <i className={`bi bi-${IconLeft} text-2xl text-(--color-texto-secundario)`} />
+         <i className={`bi bi-${IconLeft} text-md text-(--color-texto-secundario)`} />
 
         <input
         type="text"
         placeholder={Placeholder}
-        className="flex-1 bg-transparent text-lg px-4 outline-none text-(--color-texto-secundario)"
+        className="flex-1 bg-transparent text-xs px-2 outline-none text-(--color-texto-secundario)"
         />
 
         <i className={`bi bi-${IconRight} text-(--color-texto-secundario)`} />
