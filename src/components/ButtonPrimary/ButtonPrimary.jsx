15f867427d3d5c591f3fd937ad77beb1b82c5669
Santigumi <santymillan95@gmail.com>
2025-11-19 00:39:27 -0500

    onClick function added

--- src/components/ButtonPrimary/ButtonPrimary.jsx
+++ src/components/ButtonPrimary/ButtonPrimary.jsx
@@ -1,10 +1,13 @@
-
-const ButtonPrimary = ({text}) => {
-    return(
-        <button className="ml-6 px-6 py-2 rounded-lg bg-(--color-buttons-primary-green-default) cursor-pointer
-        hover:bg-(--color-buttons-primary-green-hover) transition-colors text-(--color-elementos-claros) font-medium">
-        {text}
-        </button>
-    )
-}
-export default ButtonPrimary
+const ButtonPrimary = ({ text, onClick, disabled }) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className="ml-6 px-6 py-2 rounded-lg bg-(--color-buttons-primary-green-default) cursor-pointer
+        hover:bg-(--color-buttons-primary-green-hover) transition-colors text-(--color-elementos-claros) font-medium"
+    >
+      {text}
+    </button>
+  );
+};
+export default ButtonPrimary;
