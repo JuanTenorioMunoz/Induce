a1850cbc46ddb31abc5c30039e2e74efeb5843ff
Santigumi <santymillan95@gmail.com>
2025-11-18 21:39:58 -0500

    update styles

--- src/components/Titule/Titule.jsx
+++ src/components/Titule/Titule.jsx
@@ -1,10 +1,14 @@
+
+
 const Heading = ({titule, subtitule}) => {
     return(
-        <div>
+        <div className="flex flex-col gap-3"> 
             <p 
-            className="text-4xl font-bold text-(--color-elementos-claros)"
+            className="text-center items-center text-3xl font-bold text-(--color-elementos-claros)"
             >{titule}</p>
-            <p>{subtitule}</p>
+            <p
+            className="text-center"
+            >{subtitule}</p>
         </div>
     )
 }
