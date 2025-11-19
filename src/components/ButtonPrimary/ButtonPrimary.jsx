42971ef71d83a652c08d473c1d61562ef6061ef0
Santigumi <santymillan95@gmail.com>
2025-11-18 21:36:29 -0500

    update styles

--- src/components/ButtonPrimary/ButtonPrimary.jsx
+++ src/components/ButtonPrimary/ButtonPrimary.jsx
@@ -1,7 +1,7 @@
 
 const ButtonPrimary = ({text}) => {
     return(
-        <button className="ml-6 px-6 py-2 rounded-lg bg-(--color-buttons-primary-green-default) 
+        <button className="ml-6 px-6 py-2 rounded-lg bg-(--color-buttons-primary-green-default) cursor-pointer
         hover:bg-(--color-buttons-primary-green-hover) transition-colors text-(--color-elementos-claros) font-medium">
         {text}
         </button>
