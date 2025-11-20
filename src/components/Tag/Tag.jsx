4038d1d8c06af4bab23f9beab7204fb7e051c7bc
Santigumi <santymillan95@gmail.com>
2025-11-20 17:32:02 -0500

    colors sintaxis fixed

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 
 const Tag = ({ name }) => {
   return (
-    <span className="bg-[var(--color-violet_blue_100)] text-[var(--color-violet_blue_500)] font-[Mulish] font-semibold text-sm px-3 py-1 rounded-md">
+    <span className="bg-(--color-violet-blue-100) text-(--color-violet-blue-500) font-[Mulish] font-semibold text-sm px-3 py-1 rounded-md">
       {name}
     </span>
   );
