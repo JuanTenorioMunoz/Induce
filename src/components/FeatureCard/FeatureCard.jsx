ec6ab9f34bb9d3714300d3b7f504b1ad56636a5d
Santigumi <santymillan95@gmail.com>
2025-11-20 15:43:09 -0500

    created FeatureCard component

---
+++ src/components/FeatureCard/FeatureCard.jsx
@@ -0,0 +1,10 @@
+const FeatureCard = ({titule, text}) => {
+    return(
+        <div className="flex flex-col shadow-sm border-(--color-violet-blue-200) rounded-2xl p-3">
+            <p className="font-bold">{titule}</p>
+            <p>{text}</p>
+        </div>
+    )
+}
+
+export default FeatureCard
