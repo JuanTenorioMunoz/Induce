98dcbe5af7255c3f5e2b80b8dedc528bc08069c3
Santigumi <santymillan95@gmail.com>
2025-11-20 17:32:55 -0500

    component created badge

---
+++ src/components/Badge/Badge.jsx
@@ -0,0 +1,12 @@
+
+const Snack = ({text}) => {
+    return(
+        <div
+        className="bg-(--color-violet-blue-200) rounded-full py-1 px-3 text-md"
+        >
+        <p>{text}</p>
+        </div>
+    )
+}
+
+export default Snack
