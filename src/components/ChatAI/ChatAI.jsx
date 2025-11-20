b275e7553a2ab9dbbbe2e1f4e19e83fa997f2bc1
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-11-19 21:55:05 -0500

    change user id

--- src/components/ChatAI/ChatAI.jsx
+++ src/components/ChatAI/ChatAI.jsx
@@ -54,7 +54,7 @@ const ChatAI = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           chatId,
-          id: 84,
+          id: 24,
           message: msgText,
           route: ChatWidgetConfig.webhook.route,
         }),
