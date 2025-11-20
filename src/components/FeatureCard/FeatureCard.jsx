e56dcd4485dffec2709ff26bcc1c56b7cdd380c1
Santigumi <santymillan95@gmail.com>
2025-11-20 17:31:13 -0500

    reduce text size

--- src/components/FeatureCard/FeatureCard.jsx
+++ src/components/FeatureCard/FeatureCard.jsx
@@ -2,7 +2,7 @@ const FeatureCard = ({titule, text}) => {
     return(
         <div className="flex flex-col shadow-sm border-(--color-violet-blue-200) rounded-2xl p-3">
             <p className="font-bold">{titule}</p>
-            <p>{text}</p>
+            <p className="text-sm">{text}</p>
         </div>
     )
 }
