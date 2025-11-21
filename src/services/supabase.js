3576181d40e3e225e80a29c9205d9ddf87ff6474
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-11-21 00:20:28 -0500

    fix vercel env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-export function getSupabase() {
+function getSupabase() {
   return createClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_ANON_KEY
   );
 }
 
-const supabase = getSupabase();
+export const supabase = getSupabase();
