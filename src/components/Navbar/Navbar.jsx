11f9340d0df21a5e24409700130673d3e804a611
Santigumi <santymillan95@gmail.com>
2025-11-21 11:58:35 -0500

    color sintaxis fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
+import React from "react";
 
-const NavBar = ({ title, user }) => {
+
+const NavBar = ({ title = "Inicio", user }) => {
   return (
-    <header className="w-full flex items-center justify-between px-6 py-4 bg-white">
+    <header className="w-full flex items-center justify-between px-6 py-4 border-b bg-white">
       <div className="flex items-center gap-6">
-          <p className="font-bold text-2xl text-(--color-violet-blue)">
-            {title}
-          </p>
+        <h2 className="font-outfit text-2xl text-(--color-texto-titulos_y_destacado)">
+          {title}
+        </h2>
       </div>
 
       <div className="flex items-center gap-4">
         <button
           aria-label="Notificaciones"
-          className="w-10 h-10 rounded-full flex items-center justify-center hover:bg-(--color-violet-blue-100)"
+          className="w-10 h-10 rounded-full flex items-center justify-center hover:bg-(--color-violet_blue_100)"
         >
           {/* <IoNotificationsOutline className="text-[var(--color-violet_blue)]" /> */}
         </button>
@@ -24,7 +26,7 @@ const NavBar = ({ title, user }) => {
               className="w-10 h-10 rounded-full object-cover border-2 border-(--color-chartreuse)"
             />
           ) : (
-            <div className="w-10 h-10 rounded-full bg-(--color-violet-blue) flex items-center justify-center text-white">
+            <div className="w-10 h-10 rounded-full bg-(--color-violet_blue) flex items-center justify-center text-white">
               {user?.name?.charAt(0)}
             </div>
           )}
@@ -33,7 +35,7 @@ const NavBar = ({ title, user }) => {
             <div className="text-sm font-medium text-(--color-texto-principal)">
               {user?.name}
             </div>
-            <div className="text-xs text-(--color-texto-secundario)">
+            <div className="text-xs text-(--color-texto_secundario)">
               {user?.role}
             </div>
           </div>
